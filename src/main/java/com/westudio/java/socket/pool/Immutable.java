3bc2eccd4571d9f4bfd6f113867a861f513728da
Tony <coolhzb@163.com>
2014-07-19 22:09:05 +0800

    remove the useless immutable annotation….

--- src/main/java/com/westudio/java/socket/pool/Immutable.java
+++
@@ -1,9 +0,0 @@
-package com.westudio.java.socket.pool;
-
-import java.lang.annotation.*;
-
-@Documented
-@Target(ElementType.TYPE)
-@Retention(RetentionPolicy.CLASS)
-public @interface Immutable {
-}
